2427253d2e64b707f990c2c7dd9e7cde2d10c858
jmeza777 <super0268@gmail.com>
2023-05-07 19:58:25 -0700

    changes to app.js

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="./stopwatch.css">
-    <title>Stopwatch</title>
-</head>
-<body>
-    <div class="container">
-        <div id="timer">
-            00:00:00
-        </div>
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
 
-        <div class="buttons">
-            <button id="startStopBtn">
-                <i class="material-icons" id="play">play_arrow</i>
-            </button>
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-            <button id="resetBtn">
-                <i class="material-icons" id="reset">replay</i>
-                </button>
-        </div>
-        </div>
-</body>
-<script src="./stopwatch.js"></script>
-</html>
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+let timerInterval = null;
+let timerStatus = "stopped";
+// let play = document.querySelector('#play');
+// let reset = documnet.querySelector('#reset');
+
+
+
+function stopWatch() {
+
+    seconds++
+
+    if(seconds / 60 === 1) {
+        seconds = 0;
+        minutes++;
+
+        if(minutes / 60 === 1) {
+            minutes = 0;
+            hours++
+        }
+    }
+    if(seconds < 10) {
+        leadingSeconds = "0" + seconds.toString();
+    } else {
+        leadingSeconds = seconds;
+    }
+    if( minutes < 10) {
+        leadingMinutes = "0" + minutes.toString();
+    } else {
+        leadingMinutes = minutes;
+    }
+    if(hours < 10) {
+        leadingHours = "0" + hours.toString();
+    } else {
+        leadingHours = hours;
+    }
+
+    let displayTimer = document.getElementById('timer').innerText = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
+}
+
+// window.setInterval(stopWatch, 1000)
+
+startStopBtn.addEventListener('click', function(){
+    if(timerStatus === "stopped") {
+        timerInterval =  window.setInterval(stopWatch, 1000);
+        document.getElementById('startStopBtn').innerHTML = `<i class="material-icons" id="pause">pause</i>`;
+        timerStatus = "started";
+    } else {
+        window.clearInterval(timerInterval);
+        document.getElementById('startStopBtn').innerHTML = `<i class="material-icons" id="play">play_arrow</i>`;
+        timerStatus = "stopped";  
+    }
+});
+
+resetBtn.addEventListener('click', function(){
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById('timer').innerHTML = "00:00:00";
+})
